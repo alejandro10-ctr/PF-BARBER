4dc946a24ecd33d8f84195ebb7581a533e732b1c
yeiberey <yeiberey@gmail.com>
2022-10-12 17:34:28 -0500

    Nueva endpoint

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router, json } = require('express');
 // import all routers;
 const userMiddleware = require('../middlewares/users');
+const serviceMiddleware = require('../middlewares/service');
 
 
 const router = Router();
 router.use(json())
 router.use('/users', userMiddleware);
+router.use('/services', serviceMiddleware);
 
 module.exports = router;
