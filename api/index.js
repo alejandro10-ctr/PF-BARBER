4d1419a1cb240320812730b5c7b6255ddd995ba1
yeiberey <yeiberey@gmail.com>
2022-10-11 22:04:56 -0500

    se ajusto el puerto

--- api/index.js
+++ api/index.js
@@ -21,8 +21,9 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
+
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3000"); // eslint-disable-line no-console
+    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
