763cbef410708b5243cfc8a4d3c866933907a496
yeiberey <yeiberey@gmail.com>
2022-10-12 18:40:11 -0500

    Nuevo endpoint /schedules

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router, json } = require('express');
 // import all routers;
 const userMiddleware = require('../middlewares/users');
 const serviceMiddleware = require('../middlewares/service');
+const scheduleMiddleware = require('../middlewares/schedule');
 
 
 const router = Router();
 router.use(json())
 router.use('/users', userMiddleware);
 router.use('/services', serviceMiddleware);
+router.use('/schedules',scheduleMiddleware)
 
 module.exports = router;
