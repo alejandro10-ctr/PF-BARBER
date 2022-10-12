677749ade42e33344231f65340f349cd55a1354b
yeiberey <yeiberey@gmail.com>
2022-10-11 22:08:34 -0500

    fix/controlers/getAllUsers.js

--- api/src/controllers/getAllUsers.js
+++ api/src/controllers/getAllUsers.js
@@ -1,19 +1,25 @@
 const { QueryTypes, Sequelize } = require('sequelize');
 const { User, conn, Op } = require('../db.js');
 
-const JSONUsers = require('../Data/users.json')
-
 const getDBUsers = async () => {
 
     let db = await User.findAll()
     if (!db.length) {
-        db = await User.bulkCreate(JSONUsers)
+        db = await User.bulkCreate([{
+            name: "yeiber",
+            lastname: "reyes",
+            birthday: "22-04",
+            email:"yeiberey@gmail.com",
+            password: "123456",
+            phone: "57-3203301329"
+        }])
     }
     return db
 }
 const getDBUserByPk = async id => {
     const db = await User.findAll({
-        where:{id},})
+        where: { id },
+    })
     return db
 }
 module.exports = {
