7819ed4f072ce4fe66b789c995cc8f3071645e0f
yeiberey <yeiberey@gmail.com>
2022-10-13 17:02:09 -0500

    fix/nueva funcion

--- api/src/middlewares/getAllServices.js
+++ api/src/middlewares/getAllServices.js
@@ -1,16 +1,59 @@
 const { QueryTypes, Sequelize } = require('sequelize');
-const { Service, conn, Op } = require('../db.js');
+const { Service, Schedule, Day, Hour, conn, Op } = require('../db.js');
 
 const JSONServices = require('../Data/services.json')
 
 const getDBServices = async () => {
 
-    let db = await Service.findAll()
+    let db = await Service.findAll({
+        include: {
+            model: Schedule,
+            include: {
+                model: Day,
+                include: Hour
+            }
+        }
+    })
     if (!db.length) {
-        db = await Service.bulkCreate(JSONServices,{validate:true})
+        db = await Service.bulkCreate(JSONServices, { validate: true })
     }
     return db
 }
+
+const getDBServiceByPk = async id => {
+    if(id){
+        let db = await Service.findAll({
+            where:{
+                id
+            },
+            include: {
+                model: Schedule,
+                include: {
+                    model: Day,
+                    include: Hour
+                }
+            }
+        })
+        if (!db.length) {
+            throw new Error('service not found')
+        }
+    
+        return db[0];
+    }else{
+        throw new Error('missing serviceId')
+    }
+}
+const getDBServiceCreate = async (body) => {
+    const {name , price , available , time} = body
+    if (name && price && available && time) {
+        return await Service.create(body)
+    } else {
+        throw new Error('missing params')
+    }
+}
 module.exports = {
     getDBServices,
+    getDBServiceByPk,
+    getDBServiceCreate,
+
 }
