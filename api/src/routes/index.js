70738970ae030627138215ff88fc45d26e9a7e8a
yeiberey <yeiberey@gmail.com>
2022-10-13 10:24:04 -0500

    fix: variables renombradas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,14 @@
 const { Router, json } = require('express');
 // import all routers;
-const userMiddleware = require('../controllers/users');
-const serviceMiddleware = require('../controllers/service');
-const scheduleMiddleware = require('../controllers/schedules');
+const userController = require('../controllers/users');
+const serviceController = require('../controllers/service');
+const scheduleController = require('../controllers/schedules');
 
 
 const router = Router();
 router.use(json())
-router.use('/users', userMiddleware);
-router.use('/services', serviceMiddleware);
-router.use('/schedules',scheduleMiddleware)
+router.use('/users', userController);
+router.use('/services', serviceController);
+router.use('/schedules',scheduleController)
 
 module.exports = router;
