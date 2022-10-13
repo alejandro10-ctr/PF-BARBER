2bf453524ddcfc7a7563d0f9950939d218c5f699
yeiberey <yeiberey@gmail.com>
2022-10-13 16:59:56 -0500

    fix/nuevas endpoint

--- api/src/controllers/service.js
+++ api/src/controllers/service.js
@@ -1,11 +1,31 @@
 const { Router } = require('express');
-const { getDBServices } = require('../middlewares/getAllServices.js')
+const { getDBServices, getDBServiceByPk, getDBServiceCreate } = require('../middlewares/getAllServices.js')
 const router = Router();
 
 router.get('/', async (req, res) => {
     try {
         const allServices = await getDBServices()
         res.status(200).json(allServices)
+    } catch (error) {
+        console.log(error)
+        res.status(404).send(error)
+    }
+})
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const service = await getDBServiceByPk(id)
+        res.status(200).json(service)
+    } catch (error) {
+        console.log(error)
+        res.status(404).send('service not found')
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const serviceCreated = await getDBServiceCreate(req.body)
+        res.status(200).json(serviceCreated)
     } catch (error) {
         console.log(error)
         res.status(404).send(error)
