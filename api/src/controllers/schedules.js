e78b2170554a25cd575878cc5383c50fe0b6ee64
yeiberey <yeiberey@gmail.com>
2022-10-13 17:00:23 -0500

    fix/nuevas endpoint

--- api/src/controllers/schedules.js
+++ api/src/controllers/schedules.js
@@ -1,28 +1,45 @@
 const { Router } = require('express');
-const { getDBUsers} = require('../middlewares/getAllUsers')
-const { getDBServices} = require('../middlewares/getAllServices')
-const { getDBDays, dbCreateDay} = require('../middlewares/getAllDays.js')
-const { getDBSchedules} = require('../middlewares/getAllSchedules.js')
+const { getDBServiceByPk } = require('../middlewares/getAllServices')
+const { dbCreateDay, dbUpdateDay } = require('../middlewares/getAllDays.js')
+const { getDBSchedules, dbUpdateSchedule } = require('../middlewares/getAllSchedules.js')
 
 const router = Router();
 router.get('/', async (req, res) => {
     try {
-        await getDBServices()
-        const apiInfo = await getDBSchedules()
-        res.status(200).json(apiInfo)
+        const allSchedules = await getDBSchedules()
+        res.status(200).json(allSchedules)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
+router.post('/', async (req, res) => {
+    try {
+        const service = await getDBServiceByPk(req.body.serviceId)
+        const createdDay = await dbCreateDay(req.body, service)
+        res.status(200).send(createdDay)
     } catch (error) {
         console.log(error)
         res.status(404).send(error)
     }
 })
-router.post('/', async (req,res) =>{
+router.put('/:id/schedule', async (req, res) => {
     try {
-        await getDBServices()
-        const createdDay = await dbCreateDay(req.body)
-        res.status(200).send(createdDay)
+        const service = await getDBServiceByPk(req.params.id)
+        const updatedSchedule = await dbUpdateSchedule(req.body, service)
+        res.status(200).send(updatedSchedule)
     } catch (error) {
         console.log(error)
         res.status(404).send(error)
+    }
+})
+router.put('/:id/schedule/:day', async (req, res) => {
+    try {
+        const service = await getDBServiceByPk(req.params.id)
+        const updatedDay = await dbUpdateDay(req.body,req.params.day, service)
+        res.status(200).send(updatedDay)
+    } catch (error) {
+        console.log(error)
+        res.status(404).json(error)
     }
 })
 module.exports = router;
