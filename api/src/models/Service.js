5aae5f8cf8972c06e0d7f747e5d46d804edeff92
yeiberey <yeiberey@gmail.com>
2022-10-13 13:24:20 -0500

    fix/atributo descripción

--- api/src/models/Service.js
+++ api/src/models/Service.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
     },
     description: {
       type: DataTypes.TEXT,
-      allowNull: false,
+      valueDefault: ""
     },
     price: {
       type: DataTypes.INTEGER,
