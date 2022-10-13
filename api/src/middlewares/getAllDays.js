63a20026db949f0672eef7bc724d5751928f839b
yeiberey <yeiberey@gmail.com>
2022-10-13 17:00:41 -0500

    fix/nuevas funciones

--- api/src/middlewares/getAllDays.js
+++ api/src/middlewares/getAllDays.js
@@ -4,26 +4,70 @@ const getDBDays = async () => {
     let db = await Day.findAll()
     return db
 }
-const dbCreateDay = async ({ code, state, first, last, serviceId }) => {
-    const [schedule, createdSchedule] = await Schedule.findOrCreate({
-        where: { serviceId },
-        defaults: {
-            state: true
+const dbCreateDay = async (body, service) => {
+    const { code, first, last } = body
+    if (code.length && first && last) {
+        const [schedule, createdSchedule] = await Schedule.findOrCreate({
+            where: { id: service.scheduleId },
+            defaults: {
+                state: true
+            }
+        })
+        if (createdSchedule) {
+            service.scheduleId = schedule.id
+            service.save()
         }
-    })
-    const name = code === 'dom' ? 'domingo' : code === 'lun' ? 'Lunes' : code === 'mar' ? 'Martes' : code === 'mie' ? 'Miércoles' : code === 'jue' ? 'Juevez' : code === 'vie' ? 'Viernes' : 'Sábado'
+        const name = (code) => {
+            return code === 'dom' ? 'domingo' : code === 'lun' ? 'Lunes' : code === 'mar' ? 'Martes' : code === 'mie' ? 'Miércoles' : code === 'jue' ? 'Juevez' : code === 'vie' ? 'Viernes' : 'Sábado'
+        }
+        const findCreateDay = async (code) => {
+            const [day] = await Day.findOrCreate({
+                where: { code, scheduleId: schedule.id },
+                defaults: { name: name(code), state: true }
+            })
+            await Hour.findOrCreate({
+                where: { first, last, dayId: day.id }
+            })
+            return day
+        }
 
-    const [day, createdDay] = await Day.findOrCreate({
-        where: { code, scheduleId: schedule.id },
-        defaults: { name, state: true }
-    })
-    const [firstHour, firstMin, lastHour, lastMin] = [Math.round(first.substr(0, 2)), Math.round(first.substr(2, 2)), Math.round(last.substr(0, 2)), Math.round(last.substr(2, 2))]
-    const [hour] = await Hour.findOrCreate({
-        where:{first, last, dayId: day.id }
-    })
-    return hour
+
+        if (typeof code === "string") {
+            const day = await findCreateDay(code)
+            return `day ${code} created`
+        } else {
+            code.map(async code => {
+                const day = await findCreateDay(code)
+            })
+            return `days ${code.length > 2 ?code.splice(0,code.length-2).join(', ')+", "+code[code.length-2]+" and "+code[code.length-1]:code.length === 2?code[0]+" and "+code[1]:code[0]} created`
+        }
+    } else {
+        console.log('missing params')
+        throw new Error('missing params')
+    }
+}
+const dbUpdateDay = async (body, code,service) => {
+    const { state } = body
+    if (typeof state !== "undefined") {
+        let day = await Day.findAll({
+            where:{
+                scheduleId: service.scheduleId,
+                code 
+            }
+        })
+        if(day.length){
+            day = day[0]
+            day.state = state
+            day.save()
+            return `updated schedule id:${code}`
+        }else{
+            throw new Error('day not found')
+        }
+    } else {
+        throw new Error('missing params')
+    }
 }
 module.exports = {
-    getDBDays,
     dbCreateDay,
+    dbUpdateDay,
 }
