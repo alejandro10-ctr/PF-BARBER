c2431e6c21ae921117bd62c1f97a45eef0972358
yeiberey <yeiberey@gmail.com>
2022-10-13 17:01:26 -0500

    fix/nuevos metodos

--- api/src/middlewares/getAllSchedules.js
+++ api/src/middlewares/getAllSchedules.js
@@ -1,14 +1,27 @@
 const { QueryTypes, Sequelize } = require('sequelize');
-const { Schedule, conn, Op } = require('../db.js');
-const JSONSchedules = require('../Data/schedules.json')
+const { Schedule, Day, Hour, conn, Op } = require('../db.js');
 const getDBSchedules = async () => {
-    let db = await Schedule.findAll()
-    if (!db.length) {
-        console.log(db)
-        db = await Schedule.bulkCreate(JSONSchedules)
-    }
+    let db = await Schedule.findAll({
+
+        include: {
+            model: Day,
+            include: Hour
+        }
+    })
     return db
+}
+const dbUpdateSchedule = async (body, service) => {
+    const { state } = body
+    if (typeof state !== "undefined") {
+        let schedule = await Schedule.findByPk(service.scheduleId)
+        schedule.state = state
+        schedule.save()
+        return `updated schedule id:${service.scheduleId}`
+    } else {
+        throw new Error('missing params')
+    }
 }
 module.exports = {
     getDBSchedules,
+    dbUpdateSchedule,
 }
