701a70b06b1cb3295a7364c80386f0eca196afca
yeiberey <yeiberey@gmail.com>
2022-10-13 20:37:08 -0500

    fix/endpoints completed

--- api/src/controllers/schedules.js
+++ api/src/controllers/schedules.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const { getDBServiceByPk } = require('../middlewares/getAllServices')
-const { dbCreateDay, dbUpdateDay } = require('../middlewares/getAllDays.js')
-const { getDBSchedules, dbUpdateSchedule } = require('../middlewares/getAllSchedules.js')
+const { getDBSchedules, dbUpdateSchedule, dbDeleteSchedule } = require('../middlewares/getAllSchedules.js')
+const { dbCreateDay, dbUpdateDay, dbDeleteDay } = require('../middlewares/getAllDays.js')
+const { dbUpdateHour, dbDeleteHour } = require('../middlewares/getAllHours.js')
 
 const router = Router();
 router.get('/', async (req, res) => {
@@ -9,7 +10,7 @@ router.get('/', async (req, res) => {
         const allSchedules = await getDBSchedules()
         res.status(200).json(allSchedules)
     } catch (error) {
-        res.status(404).send(error)
+        res.status(404).send(error.message)
     }
 })
 router.post('/', async (req, res) => {
@@ -18,28 +19,60 @@ router.post('/', async (req, res) => {
         const createdDay = await dbCreateDay(req.body, service)
         res.status(200).send(createdDay)
     } catch (error) {
-        console.log(error)
-        res.status(404).send(error)
+        res.status(404).send(error.message)
     }
 })
-router.put('/:id/schedule', async (req, res) => {
+router.put('/:serviceId', async (req, res) => {
     try {
-        const service = await getDBServiceByPk(req.params.id)
+        const service = await getDBServiceByPk(req.params.serviceId)
         const updatedSchedule = await dbUpdateSchedule(req.body, service)
         res.status(200).send(updatedSchedule)
     } catch (error) {
-        console.log(error)
-        res.status(404).send(error)
+        res.status(404).send(error.message)
     }
 })
-router.put('/:id/schedule/:day', async (req, res) => {
+router.delete('/:serviceId', async (req, res) => {
     try {
-        const service = await getDBServiceByPk(req.params.id)
-        const updatedDay = await dbUpdateDay(req.body,req.params.day, service)
+        const service = await getDBServiceByPk(req.params.serviceId)
+        await dbDeleteSchedule(req.params.serviceId, service)
+        res.status(200).send("deleted successfully")
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+router.put('/:serviceId/:code', async (req, res) => {
+    try {
+        const service = await getDBServiceByPk(req.params.serviceId)
+        const updatedDay = await dbUpdateDay(req.body, req.params, service)
         res.status(200).send(updatedDay)
     } catch (error) {
-        console.log(error)
-        res.status(404).json(error)
+        res.status(404).json(error.message)
+    }
+})
+router.delete('/:serviceId/:code', async (req, res) => {
+    try {
+        const service = await getDBServiceByPk(req.params.serviceId)
+        await dbDeleteDay(req.params.code, service)
+        res.status(200).send("deleted successfully")
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+router.put('/:serviceId/:code/:hourId', async (req, res) => {
+    try {
+        const updatedHour = await dbUpdateHour(req.body, req.params)
+        res.status(200).send(updatedHour)
+    } catch (error) {
+        res.status(404).json(error.message)
+    }
+})
+
+router.delete('/:serviceId/:code/:hourId', async (req, res) => {
+    try {
+        await dbDeleteHour(req.params)
+        res.status(200).send("deleted successfully")
+    } catch (error) {
+        res.status(404).send(error.message)
     }
 })
 module.exports = router;
