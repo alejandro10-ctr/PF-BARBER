82e55f9f8534babc449027216b76c169354f323d
brbnoriega <barbaranoriega330@gmail.com>
2022-10-16 17:47:24 -0300

    commiteo por fede: AboutUS y Home CSS

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -1,49 +1,117 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./About.css";
 
 export default function AboutUs() {
   return (
-    <div>
-     {/*  <p>We are a hairdresser with a strong reputation among people.</p>
-      <p>Since it was created we try to have customers as a priority.</p> */}
-      <p>So..  who we are?</p>
-      <br></br>
-      <br></br>
-      <p>Do you own a hair salon? Are you facing any issue to communicate or maintain a seamless customer relationship after COVID effects? One more question, were you able to generate enough revenue from your salon during the pandemic?</p>   
-      <p> This platform is an amazing beauty and grooming marketplace which connects the clients to the barber professionals of their choice. </p>
-      <p>Not only eases the way customers get beauty services but also allow the salon owners to target a huge segment of the market.</p>
-      <p> Besides that, you can reach a new audience with the help of an online platform. During this pandemic time, people got more comfortable with the online services, therefore by digitizing your salon, you can achieve your estimated goal easily.</p>
-      <p>Developing a software for a hair salon is one of the best ways to increase the revenue in this world of digitalization.</p>
-      <p> You can also buy hair's products and pay them threw this website's personal ecommerce.</p>
-      <p> In short, the timings and services are being planned by the customers at their preferences. </p>
-      <p> The online destination to get the most professional beauty services from the top profesionals in the market.</p>
-      <p>So, now you might be clear on why you need this software for the saloon.</p>
-      <p> Follow simple steps like a book, pay, tip and rebook.</p>
-      
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <div>
         <div>
-          <h4>Contact us</h4>
-        </div>
-        {/* * * * * * * COMPLETAR LOS MAILS Y NOMBRES DE CADA UNO * * * * * * * *  */}
-        <Link to="mail">Barbara Noriega</Link>
-        <br></br>
-        <Link to="mail">Yeiber Reyes</Link>
-        <br></br>
-        <Link to="mail">FedeSaffo</Link>
-        <br></br>
-        <Link to="mail">Fabri</Link>
-        <br></br>
-        <Link to="mailto:martinhorodeski@gmail.com">Martin Horodeski</Link>
-        <br></br>
-        <Link to="mail">Alejandro Diez</Link> 
-        <br></br>
-        <br></br>
-        <br></br>
+          <h1 className="TITLE">ABOUT US</h1>
+          <div className="TarGitHub">
+            <br />
+            <h3>DEVELOPERS GITHUB</h3>
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://github.com/Yeiberey"
+              className="Target"
+            >
+              YEIBER
+            </a>
+            <br />
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://github.com/brbnoriega"
+              className="Target"
+            >
+              BARBI
+            </a>
+            <br />
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://github.com/HORO98"
+              className="Target"
+            >
+              MARTIN
+            </a>
+            <br />
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://github.com/AlejandroDiez"
+              className="Target"
+            >
+              ALEJANDRO
+            </a>
+            <br />
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://github.com/FedeSaffores"
+              className="Target"
+            >
+              FEDE
+            </a>
+            <br />
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://github.com/fbrz1"
+              className="Target"
+            >
+              FABRI
+            </a>
+            <br />
+          </div>
+          <div className="texto">
+        <p>
+          Do you own a hair salon? Are you facing any issue to communicate or
+          maintain a seamless customer relationship after COVID effects? One
+          more question, were you able to generate enough revenue from your
+          salon during the pandemic?
+        </p>
+        <p>
+          {" "}
+          This platform is an amazing beauty and grooming marketplace which
+          connects the clients to the barber professionals of their choice.{" "}
+        </p>
+        <p>
+          Not only eases the way customers get beauty services but also allow
+          the salon owners to target a huge segment of the market.
+        </p>
+        <p>
+          {" "}
+          Besides that, you can reach a new audience with the help of an online
+          platform. During this pandemic time, people got more comfortable with
+          the online services, therefore by digitizing your salon, you can
+          achieve your estimated goal easily.
+        </p>
+        <p>
+          Developing a software for a hair salon is one of the best ways to
+          increase the revenue in this world of digitalization.
+        </p>
+        <p>
+          {" "}
+          You can also buy hair's products and pay them threw this website's
+          personal ecommerce.
+        </p>
+        <p>
+          {" "}
+          In short, the timings and services are being planned by the customers
+          at their preferences.{" "}
+        </p>
+        <p>
+          {" "}
+          The online destination to get the most professional beauty services
+          from the top profesionals in the market.
+        </p>
+        <p>
+          So, now you might be clear on why you need this software for the
+          saloon.
+        </p>
+        <p> Follow simple steps like a book, pay, tip and rebook.</p>
       </div>
     </div>
-  );
-}
+  ); 
+      }
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import Carrusel from "../carrusel/carrusel";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div>
-      {" "}
-      <Carrusel />{" "}
+      <div className="titulo">
+        <img
+          src="https://w7.pngwing.com/pngs/139/83/png-transparent-scissors-angle-technic-scissors.png"
+          className="tijeraImgen"
+        ></img>
+        <h1>BARBER 'S APP</h1>
+      </div>
+      <Carrusel />
+      <br />
+      <br />
+      <br />
+      <Link className="btn" to="/AboutUs">
+        ABOUT US
+      </Link>
+      <Link className="btn" to="">
+        SERVICES
+      </Link>
+      <Link className="btn" to="/shop">
+        PRODUCT
+      </Link>
+      <Link className="btn" to="">
+        SING UP
+      </Link>
+      <Link className="btn" to="">
+        CREATE ACCOUNT
+      </Link>
+      <br />
+      <br />
+      <br />
+      <h3>PAGA CON MERCADO DE PAGO</h3>
+      <img
+        src="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/718/448/products/d_738371-mla49316492518_032022-f1-ed4272fc0b58b093aa16519683623243-640-0.jpg"
+        width="100"
+        height="100"
+        alt="Mercado de Pago"
+      ></img>
+      <br />
+      <br />
     </div>
   );
 }
