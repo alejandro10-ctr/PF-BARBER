9ca9b58c9bdb49647d26e72dfbc51cdd8f4e064c
Alejandro Diez <alejandrodiezaceguinolaza@gmail.com>
2022-10-16 01:24:06 -0300

    feat: About Us

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -2,8 +2,14 @@ import React from "react";
 export default function AboutUs() {
   return (
     <div>
-      <p>We are a hairdresser with a strong reputation among people.</p>
-      <p>Since it was created we try to have customers as a priority.</p>
+     {/*  <p>We are a hairdresser with a strong reputation among people.</p>
+      <p>Since it was created we try to have customers as a priority.</p> */}
+
+      <p> This platform is an amazing beauty and grooming marketplace which connects the clients to the barber professionals of their choice. </p>
+      <p> You can also buy hair's products and pay them threw this website's personal ecommerce.</p>
+      <p> The online destination to get the most professional beauty services from the top profesionals in the market.</p>
+      <p> Follow simple steps like a book, pay, tip and rebook.</p>
+      
     </div>
   );
 }
