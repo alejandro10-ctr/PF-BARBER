f09dc9584195a1c9c0e6771dca568853c3bd53a6
Fabrizio <peloalder@gmail.com>
2022-10-19 15:36:18 -0300

    reducer created

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -16,7 +16,7 @@ import {
   TYPES,
   SORT_SCORE,
   FILTER_QUALITY,
-  FILTER_SHOP
+  FILTER_SHOP,
 } from "./actions";
 
 const initialState = {
@@ -28,17 +28,20 @@ const initialState = {
   cart: [],
   localStorage: [],
   filterstate: [],
-  error: '',
+  error: "",
 };
-
-
 
 export default function reducer(state = initialState, { type, payload }) {
   switch (type) {
     case SET_LOADING:
       return { ...state, ...payload };
     case GET_PRODUCTS:
-      return { ...state, allProducts: [...payload], products: [...payload], filterstate: [...payload] };
+      return {
+        ...state,
+        allProducts: [...payload],
+        products: [...payload],
+        filterstate: [...payload],
+      };
     // case CREATE_PRODUCTS:
     //   return { ...state, products: payload };
     case UPDATE_PRODUCTS:
@@ -89,66 +92,77 @@ export default function reducer(state = initialState, { type, payload }) {
     // }
     //--------------------------PRICE
     case PRICE_HIGH:
-      let stateProd = state.products
+      let stateProd = state.products;
       return {
         ...state,
-        products: stateProd.slice().sort((a, b) => {
-
-          return a.price - b.price
-        }).reverse()
-      }
+        products: stateProd
+          .slice()
+          .sort((a, b) => {
+            return a.price - b.price;
+          })
+          .reverse(),
+      };
     case PRICE_LOWER:
-      let statePr = state.products
+      let statePr = state.products;
       return {
         ...state,
         products: statePr.slice().sort((a, b) => {
-          return a.price - b.price
-        })
-
-      }
+          return a.price - b.price;
+        }),
+      };
+    case "REGISTER_USER": {
+      let newUser = state;
+    }
 
     case TYPES.ADD_TO_CART: {
-      let newItem = state.products.find(product => product.id === payload); // CHEQUEAR QUE SEA PRODUCTSTOCART.ID O PRODUCTS.ID
+      let newItem = state.products.find((product) => product.id === payload); // CHEQUEAR QUE SEA PRODUCTSTOCART.ID O PRODUCTS.ID
       // console.log(newItem)
-      let itemInCart = state.cart.find(item => item.id === newItem.id)
+      let itemInCart = state.cart.find((item) => item.id === newItem.id);
 
-      return itemInCart ? {
-        ...state,
-        cart: state.cart.map(item => item.id === newItem.id ? {
-          ...item,
-          quantity: item.quantity + 1
-        } : item)
-      }
-        :
-        {
-          ...state,
-          cart: [...state.cart, { ...newItem, quantity: 1 }],
-        }
-
+      return itemInCart
+        ? {
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === newItem.id
+                ? {
+                    ...item,
+                    quantity: item.quantity + 1,
+                  }
+                : item
+            ),
+          }
+        : {
+            ...state,
+            cart: [...state.cart, { ...newItem, quantity: 1 }],
+          };
     }
     case TYPES.REMOVE_ONE_FROM_CART: {
-      let itemToDelete = state.cart.find(item => item.id === payload);
+      let itemToDelete = state.cart.find((item) => item.id === payload);
 
-      return itemToDelete.quantity > 1 ? {
-        ...state,
-        cart: state.cart.map(item => item.id === payload ? { ...item, quantity: item.quantity - 1 } : item)
-      }
-        :
-        {
-          ...state,
-          cart: state.cart.filter(item => item.id !== payload)
-        };
+      return itemToDelete.quantity > 1
+        ? {
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === payload
+                ? { ...item, quantity: item.quantity - 1 }
+                : item
+            ),
+          }
+        : {
+            ...state,
+            cart: state.cart.filter((item) => item.id !== payload),
+          };
     }
     case TYPES.REMOVE_ALL_FROM_CART: {
       return {
         ...state,
         cart: state.cart.filter((item) => item.id !== payload),
-      }
+      };
     }
     case TYPES.CLEAR_CART:
-      return 'shoppingInitialState';
+      return "shoppingInitialState";
 
-    // case FILTER_QUALITY:    
+    // case FILTER_QUALITY:
     // const all = state.products;
     // const filter = payload === 'Premium' ? all.filter(r => r.quality === "Premium"): all.filter(r => r.quality === "Basic")
     // return {
@@ -157,51 +171,59 @@ export default function reducer(state = initialState, { type, payload }) {
     // };
     case FILTER_QUALITY:
       const all = state.allProducts;
-      const filter = payload === 'default' ? all: all.filter(r => r.quality.toLowerCase() === payload.toLowerCase())
+      const filter =
+        payload === "default"
+          ? all
+          : all.filter(
+              (r) => r.quality.toLowerCase() === payload.toLowerCase()
+            );
       return {
         ...state,
-      products: filter,
-      filterstate: filter
+        products: filter,
+        filterstate: filter,
       };
     case FILTER_SHOP:
       const allAccesory = state.filterstate;
 
-      const logicFilter = payload === 'all' ? allAccesory
-        : allAccesory.filter(r => r.name.toLowerCase().includes(payload.toLowerCase()))
+      const logicFilter =
+        payload === "all"
+          ? allAccesory
+          : allAccesory.filter((r) =>
+              r.name.toLowerCase().includes(payload.toLowerCase())
+            );
 
       return {
         ...state,
-        products: logicFilter
+        products: logicFilter,
       };
 
+    // case FILTER_SHOP:
+    //   const allAccesory = state.products;
 
-      // case FILTER_SHOP:
-      //   const allAccesory = state.products;
-        
-      //   if (allAccesory) {
-  
-      //     const logicFilter = payload === 'all' ? allAccesory
-      //       : allAccesory.filter(r => r.name.toLowerCase().includes(payload.toLowerCase()))
-      //     return {
-      //       ...state,
-      //       allProducts: logicFilter
-      //     };
-      //   } else if (!allAccesory) {
-      //     const logicFilter = payload === 'all' ? allAccesory
-      //       : allAccesory.filter(r => r.name.toLowerCase().includes(payload.toLowerCase()))
-      //     return {
-      //       ...state,
-      //       filterstate: logicFilter
-      //     };
-      //   }
-      //-------------error
-      case "FAILURE":
-        return{
-          ...state,
-          error: payload
-        }
+    //   if (allAccesory) {
+
+    //     const logicFilter = payload === 'all' ? allAccesory
+    //       : allAccesory.filter(r => r.name.toLowerCase().includes(payload.toLowerCase()))
+    //     return {
+    //       ...state,
+    //       allProducts: logicFilter
+    //     };
+    //   } else if (!allAccesory) {
+    //     const logicFilter = payload === 'all' ? allAccesory
+    //       : allAccesory.filter(r => r.name.toLowerCase().includes(payload.toLowerCase()))
+    //     return {
+    //       ...state,
+    //       filterstate: logicFilter
+    //     };
+    //   }
+    //-------------error
+    case "FAILURE":
+      return {
+        ...state,
+        error: payload,
+      };
 
     default:
       return state;
   }
-};
+}
