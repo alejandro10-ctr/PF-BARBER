529bcdbaf9431a28979b17c413556c3ffa1ce554
Fabrizio <peloalder@gmail.com>
2022-10-20 12:48:46 -0300

    solucionado el import del app

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,7 @@ import Home from "./components/Home/Home.jsx";
 import { createProducts, getProducts } from "./store/actions";
 import ShoppingCart from "./components/Shopping/ShoppingCart";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
-import Formu from "./components/Form/Form.jsx";
-
-import Form from './components/Form/Form.jsx';
-
+import Form from "./components/Form/Form.jsx";
 
 import "./App.css";
 //holi
@@ -17,33 +14,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Route exact path="/"><Home/></Route>
-        
-      <Route exact path="/shop"><Ecommerce /></Route>
-        
-      <Route exact path="/aboutus"><AboutUs /> </Route>
-        
-      <Route path='/register'><Form/></Route>
-
-      <Route exact path="/product/:id"
-        render={({match}) => {
-          return <DetailProduct match={match} />
-        }}>
-
-=======
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/register">
+        <Form />
       </Route>
       <Route exact path="/shop">
         <Ecommerce />
       </Route>
       <Route exact path="/aboutus">
         <AboutUs />
-      </Route>
-      <Route exact path="/register">
-        <Formu />
->>>>>>> 30e58f27b34816d4748c053c6a376791d6bfef42
       </Route>
 
       <Route
