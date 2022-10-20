ee9c4586646890024f736edddafb6a6adf0138fb
brbnoriega <barbaranoriega330@gmail.com>
2022-10-20 12:22:10 -0300

    form y css

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,22 @@ import { createProducts, getProducts } from "./store/actions";
 import ShoppingCart from "./components/Shopping/ShoppingCart";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
 
+import Form from './components/Form/Form.jsx';
+
+
 import "./App.css";
 //holi
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/shop">
-        <Ecommerce />
-      </Route>
-      <Route exact path="/aboutus">
-        <AboutUs />
-      </Route>
+      <Route exact path="/"><Home/></Route>
+        
+      <Route exact path="/shop"><Ecommerce /></Route>
+        
+      <Route exact path="/aboutus"><AboutUs /> </Route>
+        
+      <Route path='/register'><Form/></Route>
 
       <Route exact path="/product/:id"
         render={({match}) => {
--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,172 +1,173 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'fornik';
+import React from "react";
+import { Formik } from "formik";
 
+import styles from '../Form/Form.module.css';
 
+const initialValues = {
+  email: "",
+  password: "",
+  name: "",
+  lastname: "",
+  adress: "",
+  phone: "",
+};
 
-export default function Form() {
-    const [sendForm, setSendForm] = useState(false);
-    return (
-        <>
+const validate = (values) => {
+  let errors = {};
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-            <Formik
-                initialValues={{
-                    name: '',
-                    lastname: '',
-                    password: '', //ver 
-                    email: '',
-                    phone: '', // quizas un array??? ver
-                }}
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!regex.test(values.email)) {
+    errors.email = "Invalid Email";
+  }
 
-                validate={(valuesForm) => {
-                    let errorsCheck = {}
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 4) {
+    errors.password = "Password too short";
+  }
 
-                    //validate name:
-                    if (!valuesForm.name) {
-                        errorsCheck.name = 'Pls Write Your Name...'
-                    } else if (/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valuesForm.name)) {
-                        errorsCheck.name = 'We need your name without numbers... '
-                    }
-                    //validate lastname
-                    if (!valuesForm.lastname) {
-                        errorsCheck.lastname = 'Pls Write Your Lastname...'
-                    } else if (/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valuesForm.lastname)) {
-                        errorsCheck.lastname = 'Write your lastname without numbers... '
-                    }
+  return errors;
+};
 
-                    //validate email:
-                    if (!valuesForm.email) {
-                        errorsCheck.email = 'A email is required...'
-                    } else if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valuesForm.email)) {
-                        errorsCheck.email = 'We need a email... '
-                    }
-                    //validate password
-                    if (!valuesForm.password) {
-                        errorsCheck.password = 'Your password is required...'
-                    } else if (/^(?=.*\d)(?=.*[a-zA-Z]).{6,20}$/.test(valuesForm.password)) {
-                        errorsCheck.password = 'Minimum six characters & maximum 16 characters. At least one special character, At least one number & Special characters are optional... '
-                    }
-                    //validate phone
-                    if (!valuesForm.phone) {
-                        errorsCheck.phone = 'Your phone number is required...'
-                    } else if (/^((+|)[0-9]{1,3}(-|\s)[0-9]{2,4}(-|\s)[0-9]{6,8})$/.test(valuesForm.phone)) {
-                        errorsCheck.phone = 'Type your phone with the code area...'
-                    }
-                    return errorsCheck
-                }}
-                //funcion que se ejecuta en el submit cuando enviamos el form
-                onSubmit={(valuesForm, { resetForm }) => {
-                    resetForm(); // limpio form
-                    setSendForm(true);
-                    setTimeout(() => setSendForm(false), 5000);
-                    // alert('Formulario enviado');
-                    console.log(valuesForm);
-                }}
-            >
-                {/* importaciones de formik */}
-                {({ errors }) => (  //renderer prop dentro de la funcion entonces le inyectamos valores de fornik -> handleSubmit
-                    <Form>
-                        {/* name */}
-                        <div>
-                            <label htmlFor="name">Name</label>
-                            <Field     // el componente field permite manejar sin necesidad de pasarle las funciones coomentadas ya que por defecto hace esa logica
-                                type="text"
-                                id='name'
-                                name='name'
-                                placeholder='Your Name is....'
-                            // value={values.name}
-                            // onChange={handleChange}
-                            // onBlur={handleBlur} // cuando da click al form ejecuta funcion que valida el campo.
-                            />
-                            <ErrorMessage name='name' component={() => (
-                                <div>{errors.name}</div>
-                            )} />
-                            {/* {touched.name && errors.name && <div>{errors.name}</div>} */}
-                        </div>
+const submitForm = (values) => {
+  console.log(values);
+};
 
-                        {/*lastname*/}
-                        <div>
-                            <label htmlFor="lastname">Lastname</label>
-                            <Field
-                                type="text"
-                                id='lastname'
-                                name='lastname'
-                                placeholder='Your Lastname is....'
-                            />
-                             <ErrorMessage name='lastname' component={() => (
-                                <div>{errors.lastname}</div>
-                            )} />
-                        </div>
+const Form = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={submitForm}
+    >
+      {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty
+        } = formik;
+        return (
+          <div class="container" className={styles.background}> 
+          <div class="row">
+          <div class="col-md-6"> 
+          <div class="card">
 
-                        {/*mail*/}
-                        <div>
-                            <label htmlFor="email">Email</label>
-                            <Field
-                                type="text"
-                                id='email'
-                                name='email'
-                                placeholder='email@gmail.com....'
-                            // value={values.email}
-                            // onChange={handleChange}
-                            // onBlur={handleBlur}
-                            />
-                            <ErrorMessage name='email' component={() => (
-                                <div>{errors.email}</div>
-                            )} />
-                            {/* {touched.email && errors.email && <div>{errors.email}</div>}  si clickea el campo y clickea afuera sin hacer cambios ->touched */}
-                        </div>
+          
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit} class="box">
+              <div className="form-row">
+             
+                          
+              <label htmlFor="email">Name</label>
+                <input
+                  type="name"
+                  name="name"
+                  id="name"
+                  value={values.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.name && touched.name ? "input-error" : null
+                  }
+                />      <br />     
 
-                        {/*phone*/}
-                        <div>
-                            <label htmlFor="phone">Phone</label>
-                            <Field
-                                type="number"
-                                id='phone'
-                                name='phone'
-                                placeholder='543413307791....'
-                            // value={values.phone}
-                            // onChange={handleChange}
-                            // onBlur={handleBlur}
-                            />
-                            <ErrorMessage name='phone' component={() => (
-                                <div>{errors.phone}</div>
-                            )} />
-                            {/* {errors.phone && <div>{errors.phone}</div>} */}
-                        </div>
+                <label htmlFor="lastname">Lastname</label>
+                <input
+                  type="lastname"
+                  name="lastname"
+                  id="lastname"
+                  value={values.lastname}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.lastname && touched.lastname ? "input-error" : null
+                  }
+                />  <br />
 
-                        {/*genre*/}
-                        <div>
-                            <label><Field type='radio' name='genre' value='man' />Men</label>
-                            <label><Field type='radio' name='genre' value='woman' />Woman</label>
-                            <label><Field type='radio' name='genre' value='binarie' />Non-binary</label>
-                        </div>
+                <label htmlFor="adress">Adress</label>
+                <input
+                  type="adress"
+                  name="adress"
+                  id="adress"
+                  value={values.adress}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.adress && touched.adress ? "input-error" : null
+                  }
+                />  <br />
 
-                        {/*password*/}
-                        <div>
-                            <label htmlFor="password">Password</label>
-                            <Field
-                                type="number"
-                                id='password'
-                                name='password'
-                                placeholder='******'
-                            />
-                            {/* {errors.phone && <div>{errors.phone}</div>} */}
-                        </div>
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="phone"
+                  name="phone"
+                  id="phone"
+                  value={values.phone}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.phone && touched.phone ? "input-error" : null
+                  }
+                />  <br />
 
-                        <ErrorMessage name='password' component={() => (
-                            <div>{errors.password}</div>
-                        )} />
-
-                        {/*register*/}
-                        <div><button type='submit'>Register</button></div>
-                        {sendForm && <p>Form Successfully Sent!</p>} {/*elemento de interfaz */}
-                    </Form>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.email && touched.email ? "input-error" : null
+                  }
+                />
+                {errors.email && touched.email && (
+                  <span className="error">{errors.email}</span>
                 )}
+              </div>
 
-            </Formik>
+              <div className="form-row">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password ? "input-error" : null
+                  }
+                />
+                {errors.password && touched.password && (
+                  <span className="error">{errors.password}</span>
+                )}
+              </div>
 
+              <button
+                type="submit"
+                className={!(dirty && isValid) ? "disabled-btn" : ""}
+                disabled={!(dirty && isValid)}
+              >
+                Sign In
+              </button>
+            </form>
+            </div>
+          </div>
+          </div>
+          </div>
+        );
+      }}
+    </Formik>
+  );
+};
 
-        </>
-
-
-    )
-}
+export default Form;
