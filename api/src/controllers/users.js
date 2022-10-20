65cc396dd08dbf5bce0bc6d7542bc21ec4f32c27
yeiberey <yeiberey@gmail.com>
2022-10-19 23:39:30 -0500

    fix/controller_user: reestructurado

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -1,60 +1,55 @@
 const { Router } = require("express");
-const { User } = require("../db.js");
 const {
   getDBUsers,
   getDBUserByPk,
   getUserByName,
+  dbCreateUser,
+  dbUpdateUser,
 } = require("../middlewares/getAllUsers.js");
 const { Op } = require("sequelize");
+//<-------------
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   const { name } = req.query;
   try {
-    const apiInfo = await getDBUsers();
     if (name) {
-      const apiInfo = await getUserByName(name);
-      return res
-        .status(200)
-        .send(
-          apiInfo.length >= 1
-            ? apiInfo
-            : "No existe ningún usuario con ese nombre"
-        );
+      const userByName = await getUserByName(name);
+      res.status(200).send(userByName)
+      return
     }
-    res.status(200).json(apiInfo);
-  } catch (error) {
-    res.status(404).send(error);
-  }
-});
-router.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    await getDBUsers();
-    const user = await getDBUserByPk(id);
-    res.status(200).json(user[0]);
-  } catch (error) {
-    console.log(error);
-    res.status(404).send(error);
-  }
-});
-router.post("/", async (req, res) => {
-  const { name, lastname, email, password, phone, image, birthday } = req.body;
-
-  try {
-    const userCreated = await User.create({
-      name,
-      lastname,
-      email,
-      password,
-      phone,
-      image,
-      birthday,
-    });
-    res.json(userCreated);
+    const users = await getDBUsers();
+    res.status(200).json(users);
   } catch (error) {
     res.status(404).send(error.message);
   }
 });
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await getDBUserByPk(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    const userCreated = await dbCreateUser(req.body);
+    res.send(userCreated);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedUser = await dbUpdateUser(req.body, req.params.id);
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+
 module.exports = router;
