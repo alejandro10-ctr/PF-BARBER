3e967026826727fa524776f088a7493b17d47a1c
Fabrizio <90932471+fbrz1@users.noreply.github.com>
2022-10-22 19:50:06 -0300

    Update ShoppingCart.js

--- client/src/components/Shopping/ShoppingCart.js
+++ client/src/components/Shopping/ShoppingCart.js
@@ -26,3 +26,95 @@ export const CartProvider = ({ children }) => {
         const inCart = cartItems.find(
             (productInCart) => productInCart.product.id === detailProduct.id
         );
+
+
+        let isShowDialog = false
+        if (inCart) {
+            if (inCart.quantity + 1 <= inCart.product.stock) {
+                inCart.quantity++
+                setCartItems([...cartItems])
+                return
+            }
+            isShowDialog = true
+        } else {
+
+            if (1 <= detailProduct.stock) {
+                setCartItems([...cartItems, {
+                    id: cartItems.length + 1,
+                    quantity: 1,
+                    iva: 0,
+                    description: "",
+                    state: 2,
+                    descriptionState: "",
+                    productId: detailProduct.id,
+                    saleId: null,
+                    userId: null,
+                    product: detailProduct
+                }
+                ])
+                return
+            }
+            isShowDialog = true
+        }
+        if (isShowDialog) {
+            Swal.fire({
+                icon: "warning",
+                title: "Oops...",
+                text: "Not in stock"
+
+            })
+        }
+
+
+    }
+    console.log(cartItems)
+
+
+    const subtractItemToCart = (detailProduct) => {
+        const inCart = cartItems.find(
+            (productInCart) => productInCart.productId === detailProduct.id
+        );
+
+        if (inCart.quantity > 1) {
+            inCart.quantity--
+            setCartItems([...cartItems])
+        }
+        if (inCart.quantity - 1 === 0) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "You can buy from 1"
+            })
+        }
+    };
+    const deleteItemToCart = (detailProduct) => {
+        const inCart = cartItems.find(
+            (productInCart) => productInCart.productId === detailProduct.id
+        );
+        cartItems.splice(cartItems.indexOf(inCart),1)
+        setCartItems([...cartItems]);
+
+        if (inCart.quantity - 1 === 0) {
+            Swal.fire({
+                icon: "error",
+                title: "Successfully deleted",
+                text: `Product ${inCart.product.name} delete from cart`
+            })
+        }
+    }
+
+
+    /* if(localStorage.getItem(el => JSON.stringify(el.quantity) === "0") ){
+        localStorage.removeItem()
+      } */
+
+
+    return (
+        <CartContext.Provider value={{ cartItems, addItemToCart, subtractItemToCart, deleteItemToCart }}>
+            {children}
+        </CartContext.Provider>
+    )
+
+
+
+};
