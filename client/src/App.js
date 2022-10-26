499009c76b7d4e6cfe6b612a9a9c56db79f16130
fbrz <peloalder@gmail.com>
2022-10-26 12:21:58 -0300

    added app.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
 import DetailProduct from "./components/DetailProducts/DetailProducts";
 import Ecommerce from "./components/Ecommerce/Ecommerce";
 import Home from "./components/Home/Home.jsx";
+import { createProducts, getProducts } from "./store/actions";
+import ShoppingCart from "./components/Shopping/ShoppingCart";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
+
 import MercadoPago from "./components/MercadoPago/MercadoPago";
 import Register from "./components/Register/Register.jsx";
 import LoginUser from "./components/LoginUser/LoginUser";
@@ -41,24 +43,26 @@ function App() {
     <div className="App">
       <HomeNavBar user={user}/>
 
+
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import HomeNavBar from "./components/HomeNavBar/HomeNavBar.jsx";
+
+import "./App.css";
+//holi
+
+function App() {
+  return (
+    <div className="App">
       <Route exact path="/">
         <Home />
       </Route>
-
-      <Route exact path="/register">
-        <Register />
-      </Route>
-
-      <Route exact path="/login">
-        <LoginUser />
-      </Route>
-
       <Route exact path="/shop">
         <Ecommerce />
       </Route>
       <Route exact path="/aboutus">
         <AboutUs />
       </Route>
+
 
       <Route exact path="/payments/pay">
         <MercadoPago />
@@ -89,14 +93,31 @@ function App() {
         <ChangePassword />
       </Route>
 
+
       <Route
         exact
         path="/product/:id"
         render={({ match }) => {
           return <DetailProduct match={match} />;
         }}
+
       />
       
+
+      ></Route>
+
+      <Route exact path="/dash">
+        <Dashboard />
+      </Route>
+
+      {/* <Route path="/" render={({ location }) => {
+        return <HomeNavBar user={user} pathname={location.pathname} />
+      }}> </Route> */}
+
+      {/* <Route path = "/">
+      <HomeNavBar />
+      </Route> */}
+
     </div>
   );
 }
