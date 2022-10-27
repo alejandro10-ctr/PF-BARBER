7f49ba76164c91ad5234ee3f2c6c43eae3d8ea7c
brbnoriega <barbaranoriega330@gmail.com>
2022-10-27 17:43:17 -0300

    commit admin - back y front + start-review

--- api/src/middlewares/getAllProducts.js
+++ api/src/middlewares/getAllProducts.js
@@ -5,91 +5,104 @@ const JSONUsers = [{
   name: "Beard Balm",
   price: 200,
   quality: "premium",
-  stock: 5,
-  image: "https://http2.mlstatic.com/D_NQ_NP_2X_959303-MLA51602582272_092022-F.webp"
+  stock: 7,
+  image: "https://http2.mlstatic.com/D_NQ_NP_2X_959303-MLA51602582272_092022-F.webp",
+  score: 3,
 }, {
   name: "Mühle Razor Gillette® Fusion Vivo Series Plumtree",
   price: 5400,
   quality: "basic",
-  stock: 52,
-  image: "https://www.giftsandcare.com/12277-home_default_carousel/muehle-razor-gillette-fusion-vivo-series-plumtree.jpg"
+  stock: 6,
+  image: "https://www.giftsandcare.com/12277-home_default_carousel/muehle-razor-gillette-fusion-vivo-series-plumtree.jpg",
+  score: 4,
 },
 {
   name: "Edwin Jagger Marfil",
-  price: 200,
+  price: 4230,
   quality: "premium",
-  stock: 5,
-  image: "https://www.giftsandcare.com/9786-large_default/maquinilla-de-afeitar-clasica-plaza-edwin-jagger-marfil.jpg"
+  stock: 9,
+  image: "https://www.giftsandcare.com/9786-large_default/maquinilla-de-afeitar-clasica-plaza-edwin-jagger-marfil.jpg",
+  score: 5,
 },
 {
   name: "Brosh Super Hard Gel 200gr",
-  price: 200,
+  price: 137,
   quality: "basic",
-  stock: 5,
-  image: "https://www.giftsandcare.com/14216-large_default/brosh-super-hard-gel-200gr.jpg"
+  stock: 11,
+  image: "https://www.giftsandcare.com/14216-large_default/brosh-super-hard-gel-200gr.jpg",
+  score: 3,
 },
 {
   name: "Mühle Double Edge Safety Razor R89 Rose Gold Close Comb",
-  price: 200,
+  price: 2340,
   quality: "premium",
-  stock: 5,
-  image: "https://www.giftsandcare.com/1621-large_default/muehle-double-edge-safety-razor-r89-rose-gold-close-comb-.jpg"
+  stock: 9,
+  image: "https://www.giftsandcare.com/1621-large_default/muehle-double-edge-safety-razor-r89-rose-gold-close-comb-.jpg",
+  score: 5,
 },
 {
   name: "Fatip Chrome Slant Double Edge Safety Razor",
-  price: 200,
+  price: 820,
   quality: "premium",
-  stock: 5,
-  image: "https://www.giftsandcare.com/17239-large_default/fatip-chrome-slant-double-edge-safety-razor.jpg"
+  stock: 2,
+  image: "https://www.giftsandcare.com/17239-large_default/fatip-chrome-slant-double-edge-safety-razor.jpg",
+  score: 3,
 },
 {
   name: "Omega Garnet Shaving Bowl",
-  price: 200,
+  price: 2543,
   quality: "basic",
-  stock: 5,
-  image: "https://www.giftsandcare.com/6329-large_default/brocha-de-afeitar-pelo-sintetico-roja-omega-s10018.jpg"
+  stock: 4,
+  image: "https://www.giftsandcare.com/6329-large_default/brocha-de-afeitar-pelo-sintetico-roja-omega-s10018.jpg",
+  score: 1,
 },
 {
   name: "Fatip Piccolo Gold Slant Close Open Double Edge Safety Razor",
-  price: 200,
+  price: 987,
   quality: "basic",
-  stock: 5,
-  image: "https://www.giftsandcare.com/16511-large_default/fatip-piccolo-gold-slant-close-open-double-edge-safety-razor.jpg"
+  stock: 8,
+  image: "https://www.giftsandcare.com/16511-large_default/fatip-piccolo-gold-slant-close-open-double-edge-safety-razor.jpg",
+  score: 4,
 },
 {
   name: "Baxter of California Shave Tonic",
-  price: 200,
+  price: 776,
   quality: "premium",
-  stock: 5,
-  image: "https://www.giftsandcare.com/9869-large_default/dear-barber-shave-oil-30ml.jpg"
+  stock: 12,
+  image: "https://www.giftsandcare.com/9869-large_default/dear-barber-shave-oil-30ml.jpg",
+  score: 2,
 },
 {
   name: "Captain Fawcett Barberism Pre-Shave Oil 50ml",
-  price: 300,
+  price: 678,
   quality: "basic",
-  stock: 5,
-  image: "https://www.giftsandcare.com/9427-large_default/aceite-pre-afeitado-barberism-captain-fawcett-50ml.jpg"
+  stock: 8,
+  image: "https://www.giftsandcare.com/9427-large_default/aceite-pre-afeitado-barberism-captain-fawcett-50ml.jpg",
+  score: 5,
 },
 {
   name: "Hey Joe Pre Shave Oil 50ml",
-  price: 200,
+  price: 5655,
   quality: "premium",
-  stock: 5,
-  image: "https://www.giftsandcare.com/7783-large_default/hey-joe-pre-shave-oil-50ml.jpg"
+  stock: 7,
+  image: "https://www.giftsandcare.com/7783-large_default/hey-joe-pre-shave-oil-50ml.jpg",
+  score: 1,
 },
 {
   name: "After Shave BeardLovers",
-  price: 200,
+  price: 4545,
   quality: "basic",
   stock: 5,
-  image: "https://www.giftsandcare.com/3022-large_default/piedra-de-alumbre-natural-osma-75-gr.jpg"
+  image: "https://www.giftsandcare.com/3022-large_default/piedra-de-alumbre-natural-osma-75-gr.jpg",
+  score: 3,
 },
 {
   name: "Cella Milano Bio Aloe Vera After Shave Balm 100ml",
-  price: 200,
+  price: 655,
   quality: "premium",
-  stock: 5,
-  image: "https://www.giftsandcare.com/13418-large_default/cella-milano-bio-aloe-vera-after-shave-balm-100ml.jpg"
+  stock: 4,
+  image: "https://www.giftsandcare.com/13418-large_default/cella-milano-bio-aloe-vera-after-shave-balm-100ml.jpg",
+  score: 2,
 }
 ]
 
--- client/src/components/Dashboard/Table/Table.jsx
+++ client/src/components/Dashboard/Table/Table.jsx
@@ -1,4 +1,6 @@
 // import React from 'react'
+import { connect } from "react-redux";
+import { bindActionCreators } from 'redux'
 import '../Table/Table.scss';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,128 +10,29 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getProducts } from '../../../redux/actions';
+import img from "./img/star.png";
 
-const List = () => {
-  
-    const  rows= [{
-        id: 1,
-        name: "Beard Balm",
-        price: 200,
-        quality: "premium",
-        stock: 5,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_959303-MLA51602582272_092022-F.webp",
-        status: 'pending'
-      }, {
-        id: 2,
-        name: "Mühle Razor Gillette® Fusion Vivo Series Plumtree",
-        price: 5400,
-        quality: "basic",
-        stock: 52,
-        image: "https://www.giftsandcare.com/12277-home_default_carousel/muehle-razor-gillette-fusion-vivo-series-plumtree.jpg",
-        status: 'pending'
-    },
-      { id: 3,
-        name: "Edwin Jagger Marfil",
-        price: 200,
-        quality: "premium",
-        stock: 5,
-        image: "https://www.giftsandcare.com/9786-large_default/maquinilla-de-afeitar-clasica-plaza-edwin-jagger-marfil.jpg",
-        status: 'approved'
-      },
-      { id: 4,
-        name: "Brosh Super Hard Gel 200gr",
-        price: 200,
-        quality: "basic",
-        stock: 5,
-        image: "https://www.giftsandcare.com/14216-large_default/brosh-super-hard-gel-200gr.jpg",
-        status: 'approved'
-      },
-      { id: 5,
-        name: "Mühle Double Edge Safety Razor R89 Rose Gold Close Comb",
-        price: 200,
-        quality: "premium",
-        stock: 5,
-        image: "https://www.giftsandcare.com/1621-large_default/muehle-double-edge-safety-razor-r89-rose-gold-close-comb-.jpg",
-        status: 'pending'
-    },
-      { id: 6,
-        name: "Fatip Chrome Slant Double Edge Safety Razor",
-        price: 200,
-        quality: "premium",
-        stock: 5,
-        image: "https://www.giftsandcare.com/17239-large_default/fatip-chrome-slant-double-edge-safety-razor.jpg",
-        status: 'pending'
-    },
-      { id: 6,
-        name: "Omega Garnet Shaving Bowl",
-        price: 200,
-        quality: "basic",
-        stock: 5,
-        image: "https://www.giftsandcare.com/6329-large_default/brocha-de-afeitar-pelo-sintetico-roja-omega-s10018.jpg",
-        status: 'approved' 
-    },
-      { id: 7,
-        name: "Fatip Piccolo Gold Slant Close Open Double Edge Safety Razor",
-        price: 200,
-        quality: "basic",
-        stock: 5,
-        image: "https://www.giftsandcare.com/16511-large_default/fatip-piccolo-gold-slant-close-open-double-edge-safety-razor.jpg",
-        status: 'pending'
-      },
-      { id: 8,
-        name: "Baxter of California Shave Tonic",
-        price: 200,
-        quality: "premium",
-        stock: 5,
-        image: "https://www.giftsandcare.com/9869-large_default/dear-barber-shave-oil-30ml.jpg",
-        status: 'approved'
-      },
-      { id: 9,
-        name: "Captain Fawcett Barberism Pre-Shave Oil 50ml",
-        price: 300,
-        quality: "basic",
-        stock: 5,
-        image: "https://www.giftsandcare.com/9427-large_default/aceite-pre-afeitado-barberism-captain-fawcett-50ml.jpg",
-        status: 'pending'
-    },
-      { id: 10,
-        name: "Hey Joe Pre Shave Oil 50ml",
-        price: 200,
-        quality: "premium",
-        stock: 5,
-        image: "https://www.giftsandcare.com/7783-large_default/hey-joe-pre-shave-oil-50ml.jpg",
-        status: 'pending'
-      },
-      { id: 11,
-        name: "After Shave BeardLovers",
-        price: 200,
-        quality: "basic",
-        stock: 5,
-        image: "https://www.giftsandcare.com/3022-large_default/piedra-de-alumbre-natural-osma-75-gr.jpg",
-        status: 'approved'
-      },
-      { id: 12,
-        name: "Cella Milano Bio Aloe Vera After Shave Balm 100ml",
-        price: 200,
-        quality: "premium",
-        stock: 5,
-        image: "https://www.giftsandcare.com/13418-large_default/cella-milano-bio-aloe-vera-after-shave-balm-100ml.jpg",
-        status: 'pending'
-    }
-      ]
-      
+const List = ({ products, getProducts }) => {
+
+  useEffect(() => {
+    getProducts()
+  })
+
+
   return (
-    
+
     <TableContainer component={Paper} className='table'>
-                  <div className="datatableTitle">
-                    List products
+      <div className="datatableTitle">
+        List products
         <Link to="/dash/products/add" className="link">
-        Add New Product
-   
+          Add New Product
+
         </Link>
       </div>
-      
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell className='tableCell'>Traking ID</TableCell>
@@ -137,31 +40,34 @@ const List = () => {
             <TableCell className='tableCell'>Price</TableCell>
             <TableCell className='tableCell'>Quality</TableCell>
             <TableCell className='tableCell'>Stock</TableCell>
-            <TableCell className='tableCell'>Status</TableCell>
+            <TableCell className='tableCell'>Stars</TableCell>
+
 
 
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {products.sort((a, b) => {
+            return a.id - b.id;
+          }).map((row) => (
             <TableRow key={row.id}>
               <TableCell >{row.id}</TableCell>
               <TableCell className='tableCell'>
                 <div className='cellWrapper'>
-                    <img src={row.image} alt="" className='image' />
-                    {row.name}
+                  <img src={row.image} alt="" className='image' />
+                  {row.name}
                 </div>
               </TableCell>
-              
-           
-             
+
               <TableCell className='tableCell'>{row.price}</TableCell>
               <TableCell className='tableCell'>{row.quality}</TableCell>
               <TableCell className='tableCell'>{row.stock}</TableCell>
-              <TableCell className='tableCell'>
-                <span className={`status ${row.status}`}>{row.status}</span>
-              </TableCell>
-          
+              <TableCell className='tableCell'>{row.score === 1 ? <img src={img} alt="Img Not Found."></img>
+                : row.score === 2 ? <div><img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img></div>
+                  : row.score === 3 ? <div><img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img> </div>
+                    : row.score === 4 ? <div><img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img></div>
+                      : <div><img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img> <img src={img} alt="Img Not Found."></img></div>}</TableCell>
+
             </TableRow>
           ))}
         </TableBody>
@@ -170,4 +76,17 @@ const List = () => {
   )
 }
 
-export default List;
+
+export const mapStateToProps = ({ products, allProducts }) => {
+  return {
+    products,
+    allProducts,
+  }
+}
+export const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    getProducts,
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
