80cb73d737f08088c7eb9c563363de448e8bd77b
Horo9898 <martinhorodeski@gmail.com>
2022-10-28 20:39:09 -0300

    change product

--- client/src/components/Dashboard/SingleProduct/SingleProduct.jsx
+++ client/src/components/Dashboard/SingleProduct/SingleProduct.jsx
@@ -4,45 +4,57 @@ import Navbar from '../Navbar/Navbar';
 import '../SingleProduct/SingleProduct.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { clearDetail, getProductsDetail } from '../../../redux/actions';
 
 
 const SingleProduct = () => {
-    const {id} = useParams()
+    const { id } = useParams()
     const dispatch = useDispatch()
-   
 
+    const [edit, setEdit] = useState('false')
     useEffect(() => {
-      dispatch(getProductsDetail(id))
-      return () => dispatch(clearDetail())
+        dispatch(getProductsDetail(id))
+        return () => dispatch(clearDetail())
     }, [])
- const detail = useSelector((state)=> state.detail)
- console.log(detail)
+    const detail = useSelector((state) => state.detail)
+    console.log(detail)
+    const editTrueHandle = (e) => {
+        setEdit("true")
+        console.log(edit)
+    }
+    const editFalseHandle = (e) => {
+        setEdit("false")
+
+    }
 
     return (
-   
+
         <div className='single'>
             <Sidebar />
-            <div className='singleContainer'> 
+            <div className='singleContainer'>
                 <Navbar />
                 <div className='top'>
                     <div className='left'>
-                        <div className='editButton'>Edit</div>
                         <h1 className='title'> Information</h1>
-                        <div className='item'>     
-                            <img src={detail.image}
-                                alt="" 
-                                className='itemImg'/>
+                        {edit === "false" ?
+                            <div className='item'>
+                                <div className='editButton'>
+
+                                    <button onClick={() => editTrueHandle()}>Edit</button>
+                                </div>
+                                <img src={detail.image}
+                                    alt=""
+                                    className='itemImg' />
                                 <div className='details'>
                                     <h1 className="itemtitle">{detail.name}</h1>
                                     <div className="detailItem">
                                         <span className='itemKey'>Price:</span>
                                         <span className='itemValue'>{detail.price}</span>
                                     </div>
-                                     <div className="detailItem">
+                                    <div className="detailItem">
                                         <span className='itemKey'>Stock:</span>
-                                        <span className='itemValue'>{detail.stock}</span> 
+                                        <span className='itemValue'>{detail.stock}</span>
                                     </div>
                                     <div className="detailItem">
                                         <span className='itemKey'>Quality:</span>
@@ -52,10 +64,44 @@ const SingleProduct = () => {
                                         <span className='itemKey'>Description:</span>
                                         <span className='itemValue'>{detail.description}</span>
                                     </div>
-                                   
                                 </div>
-                        </div>
-                   
+                            </div>
+                            : <div className='item'>
+                                <div className='editButton'>
+
+                                    <button onClick={() => editFalseHandle()}>Save</button>
+                                </div>
+                                <img src={detail.image}
+                                    alt=""
+                                    className='itemImg' />
+                                <div className='details'>
+                                <h1 className="itemtitle">{detail.name}</h1>
+                                   
+                                    <div className="detailItem">
+                                        <span className='itemKey'>Price:</span>
+                                        <span className='itemValue'><input type="text"></input></span>
+                                    </div>
+                                    <div className="detailItem">
+                                        <span className='itemKey'>Stock:</span>
+                                        <span className='itemValue'><input type="text"></input></span>
+                                    </div>
+                                    <div className="detailItem">
+                                        <span className='itemKey'>Quality:</span>
+                                        <select  name="quality" >
+                                        <option value="basic">Basic</option>
+                                        <option value="premium">Premium</option>
+                                        </select>
+                                       
+
+                                    </div>
+                                    <div className="detailItem">
+                                        <span className='itemKey'>Description:</span>
+                                        <span className='itemValue'><input type="text"></input></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        }
                     </div>
                     <div className='right'></div>
                 </div>
