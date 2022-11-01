e8fd15918635d28b37a2f37b6ae59a474142be6c
brbnoriega <barbaranoriega330@gmail.com>
2022-10-31 22:11:09 -0300

    button back

--- client/src/components/Dashboard/SingleProduct/SingleProduct.jsx
+++ client/src/components/Dashboard/SingleProduct/SingleProduct.jsx
@@ -69,7 +69,7 @@ const SingleProduct = () => {
             <Sidebar />
             <div className='singleContainer'>
 
-<Link to="/dash/products"> <button class='backButton'>Back</button> </Link>
+
                    
                
                 <div className='top'>
@@ -77,7 +77,9 @@ const SingleProduct = () => {
                         <h1 className='title'> {edit === "false" ? "" : "Edit information"}</h1>
                         {edit === "false" ?
                             <div className='item'>
-                                <div >
+                                <div className='edit'>
+                                   <Link to="/dash/products"><button >Back</button> </Link>
+
                                     <button class='editButton' onClick={() => editHandle()}> <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                     </svg></button>
