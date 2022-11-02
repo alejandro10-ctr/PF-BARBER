d8c59549456660f6c07e39a101a87a9c750f3fbd
brbnoriega <barbaranoriega330@gmail.com>
2022-11-02 12:34:40 -0300

    carrusel cambios img

--- client/src/components/carrusel/carrusel.jsx
+++ client/src/components/carrusel/carrusel.jsx
@@ -11,18 +11,18 @@ import {
 <source />;
 const items = [
   {
-    src: "https://images-ext-1.discordapp.net/external/IHmjbdPQ1srrMct2wEIsWndgH8zBYkVzP4jgKXbqZoM/%3Fcs%3Dsrgb%26dl%3Dpexels-cottonbro-3998425.jpg%26fm%3Djpg/https/images.pexels.com/photos/3998425/pexels-photo-3998425.jpeg?width=396&height=594",
+    src: "https://images.pexels.com/photos/995300/pexels-photo-995300.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     altText: "Imagen 1",
     caption: "",
 
   },
   {
-    src: "https://images-ext-2.discordapp.net/external/aRgQroxSYDekLnL_FNcaYmNcbVRbxKk3a3m9v5t7KVI/%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D1260%26h%3D750%26dpr%3D1/https/images.pexels.com/photos/3993470/pexels-photo-3993470.jpeg?width=396&height=594",
+    src: "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     altText: "Imagen 2",
     caption: "",
   },
   {
-    src: "https://images-ext-1.discordapp.net/external/y5Rvako0Wa_cluT9a7qOqiMPsdCFqDOgm5KYGco9Ing/%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D600%26lazy%3Dload/https/images.pexels.com/photos/9992817/pexels-photo-9992817.jpeg?width=396&height=594",
+    src: "https://images.pexels.com/photos/8552626/pexels-photo-8552626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     altText: "Imagen 3",
     caption: "",
   },
